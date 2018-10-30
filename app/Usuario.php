f6d02f5b053dc6c402ae89c9b799128aa26ba68e
vagrant <vagrant@homestead>
2018-10-30 23:20:12 +0000

    Convertida model user para usuário

--- app/Usuario.php
+++ app/Usuario.php
@@ -2,12 +2,30 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Usuario extends Model
+class User extends Authenticatable
 {
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'nome', 'email', 'senha',
+        'name', 'email', 'password',
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
