5e7a43b4e7fadb3d74ffbe06d2f9ef71fbdf924c
Michael Santos <michael-santos@outlook.com.br>
2018-10-30 17:50:18 -0300

    Agora as routes de login estão direcionadas para homestead.teste/admin/*

---
+++ database/migrations/2018_10_29_042750_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->string('permiteInsercao');
+            $table->date('dataEvento');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
