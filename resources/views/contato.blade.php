0645b18e1d06c23bed9ee98f1170c3df8d915a65
Antonio <ajmedeiros93@gmail.com>
2018-11-01 04:36:19 -0200

    Pagina de contato pronta

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -19,7 +19,7 @@
 	<body>
 		<header>
 			<nav class="navbar navbar-dark text-white h5 navbar-expand-lg shadow" id="cabeca">
-				<a class="navbar-brand" href="#">
+				<a class="navbar-brand" href="{{url ('teste')}}">
 					<img src="{{asset ('img/placeholders/logo.png')}}" id="cabeca" alt="">
 				</a>
 
@@ -58,7 +58,40 @@
 		</header>
 
 		
-<!-- CONTAINER PRINCIPAL -->
+		<section id="contact">
+			<div class="container">
+			<div class="well well-sm">
+			<h3><strong>Contato</strong></h3>
+			</div>
+
+				<div class="row">
+				<div class="col-md-7">  
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.6444081532377!2d-47.52917808549868!3d-23.581211384672976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDM0JzUyLjQiUyA0N8KwMzEnMzcuMiJX!5e0!3m2!1spt-BR!2sbr!4v1541053313760" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+
+					<div class="col-md-5">
+					<h4><strong>Formulário para contato</strong></h4>
+						<form>
+							<div class="form-group">
+								<input type="text" class="form-control" name="" value="" placeholder="Nome">
+							</div>
+							<div class="form-group">
+								<input type="email" class="form-control" name="" value="" placeholder="E-mail">
+							</div>
+							<div class="form-group">
+								<input type="tel" class="form-control" name="" value="" placeholder="Telefone">
+							</div>
+							<div class="form-group">
+								<textarea class="form-control" name="" rows="3" placeholder="Mensagem"></textarea>
+							</div>
+							<button class="btn btn-default" type="submit" name="button">
+								<i class="fa fa-paper-plane-o" aria-hidden="true"></i> Enviar
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
 
 
 		<footer class="footer rodape">
