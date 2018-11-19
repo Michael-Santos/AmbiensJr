f31380038071856af04cd79abaf2ef5446248dc9
Michael Santos <michael-santos@outlook.com.br>
2018-11-18 23:31:21 -0200

    Admin reorganizado com blade

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -31,172 +31,38 @@
 
 <body>
     <div id="app">
-    
+        @include('layouts.admin.navbar')
 
+        <!-- Bootstrap row -->
+        <div class="row" id="body-row">
+            @include('layouts.admin.sidebar')
 
-<!-- Bootstrap NavBar -->
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">
-    <img src="https://v4-alpha.getbootstrap.com/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-    <span class="menu-collapsed">   Painel Administrativo</span>
-  </a>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Sair <span class="sr-only">(current)</span></a>
-      </li>
-      
-      <!-- This menu is hidden in bigger devices with d-sm-none. 
-           The sidebar isn't proper for smaller screens imo, so this dropdown menu can keep all the useful sidebar itens exclusively for smaller screens  -->
-      <li class="nav-item dropdown d-sm-block d-md-none">
-        <a class="nav-link dropdown-toggle" href="#" id="smallerscreenmenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Menu
-        </a>
-        <div class="dropdown-menu" aria-labelledby="smallerscreenmenu">
-            <a class="dropdown-item" href="#">Home</a>
-            <a class="dropdown-item" href="#">Perfil</a>
-            <a class="dropdown-item" href="#">Equipe</a>
-            <a class="dropdown-item" href="#">Setores</a>
-        </div>
-      </li><!-- Smaller devices menu END -->
-      
-    </ul>
-  </div>
-</nav><!-- NavBar END -->
+            <!-- MAIN -->
+            <div class="col">
+                
+                @yield('content')
 
-
-<!-- Bootstrap row -->
-<div class="row" id="body-row">
-    <!-- Sidebar -->
-    <div id="sidebar-container" class="sidebar-expanded d-none d-md-block"><!-- d-* hiddens the Sidebar in smaller devices. Its itens can be kept on the Navbar 'Menu' -->
-        <!-- Bootstrap List Group -->
-        <ul class="list-group">
-            <!-- Foto de perfil -->
-            <div class="d-flex w-100 justify-content-start align-items-center text-white block-img-perfil">
-                <img src="{{ asset('img/equipe/rodrigo.jpg') }}" alt="Rodrigo" class="img-perfil rounded-circle">
-                <span>Rodrigo<br>Admin</span>
-            </div>
-
-            <!-- Separator with title -->
-            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
-                <small>MENU PRINCIPAL</small>
-            </li>
-            <!-- /END Separator -->
-            <!-- Menu with submenu -->
-            <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-home fa-fw mr-3"></span> 
-                    <span class="menu-collapsed">Home</span>
+                <h1>
+                    Collapsing Menu
+                    <small class="text-muted">Version 2.1</small>
+                </h1>
+                
+                
+                <div class="card">
+                    <h4 class="card-header">Requirements</h4>
+                    <div class="card-body">
+                        <ul>
+                            <li>JQuery</li>
+                            <li>Bootstrap 4 beta-3</li>
+                        </ul>
+                    </div>
                 </div>
-            </a>
+               
+
+
+            </div><!-- Main Col END -->
             
-
-            <a href="#" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-user fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Perfil</span>
-                </div>
-            </a>
-
-            <!-- Separator with title -->
-            <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
-                <small>ADMINISTRAÇÂO</small>
-            </li>
-            <!-- /END Separator -->
-
-            <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-user fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Equipe</span>
-                    <span class="submenu-icon ml-auto"></span>
-                </div>
-            </a>
-            <!-- Submenu content -->
-            <div id='submenu2' class="collapse sidebar-submenu">
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Setores</span>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
-                    <span class="menu-collapsed">Membros</span>
-                </a>
-            </div>   
-
-
-            <a href="#" class="bg-dark list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-handshake-o fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Projetos</span>
-                </div>
-            </a>
-
-            <a href="#" class="bg-dark list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-calendar fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Cursos</span>
-                </div>
-            </a>
-            
-            <a href="#" class="bg-dark list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-image fa-fw mr-3"></span>
-                    <span class="menu-collapsed">Galeria</span>
-                </div>
-            </a>
-            <a href="#" data-toggle="sidebar-colapse" class="bg-dark list-group-item list-group-item-action d-flex align-items-center">
-                <div class="d-flex w-100 justify-content-start align-items-center">
-                    <span class="fa fa-file fa-fw mr-3"></span>
-                    <span id="collapse-text" class="menu-collapsed">Processo Seletivo</span>
-                </div>
-            </a>
-        </ul><!-- List Group END-->
-    </div><!-- sidebar-container END -->
-
-    <!-- MAIN -->
-    <div class="col">
-        
-        <h1>
-            Collapsing Menu
-            <small class="text-muted">Version 2.1</small>
-        </h1>
-        
-        
-        <div class="card">
-            <h4 class="card-header">Requirements</h4>
-            <div class="card-body">
-                <ul>
-                    <li>JQuery</li>
-                    <li>Bootstrap 4 beta-3</li>
-                </ul>
-            </div>
-        </div>
-       
-
-
-    </div><!-- Main Col END -->
-    
-</div><!-- body-row END -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        </div><!-- body-row END -->
     </div>
 </body>
 </html>
---
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,81 @@
+<!-- Sidebar -->
+<div id="sidebar-container" class="sidebar-expanded d-none d-md-block"><!-- d-* hiddens the Sidebar in smaller devices. Its itens can be kept on the Navbar 'Menu' -->
+    <!-- Bootstrap List Group -->
+    <ul class="list-group">
+        <!-- Foto de perfil -->
+        <div class="d-flex w-100 justify-content-start align-items-center text-white block-img-perfil">
+            <img src="{{ asset('img/equipe/rodrigo.jpg') }}" alt="Rodrigo" class="img-perfil rounded-circle">
+            <span>Rodrigo<br>Admin</span>
+        </div>
+
+        <!-- Separator with title -->
+        <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
+            <small>MENU PRINCIPAL</small>
+        </li>
+        <!-- /END Separator -->
+        <!-- Menu with submenu -->
+        <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-home fa-fw mr-3"></span> 
+                <span class="menu-collapsed">Home</span>
+            </div>
+        </a>
+        
+        <a href="#" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-user fa-fw mr-3"></span>
+                <span class="menu-collapsed">Perfil</span>
+            </div>
+        </a>
+
+        <!-- Separator with title -->
+        <li class="list-group-item sidebar-separator-title text-muted d-flex align-items-center menu-collapsed">
+            <small>ADMINISTRAÇÂO</small>
+        </li>
+        <!-- /END Separator -->
+
+        <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-user fa-fw mr-3"></span>
+                <span class="menu-collapsed">Equipe</span>
+                <span class="submenu-icon ml-auto"></span>
+            </div>
+        </a>
+        <!-- Submenu content -->
+        <div id='submenu2' class="collapse sidebar-submenu">
+            <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <span class="menu-collapsed">Setores</span>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action bg-dark text-white">
+                <span class="menu-collapsed">Membros</span>
+            </a>
+        </div>   
+
+        <a href="#" class="bg-dark list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-handshake-o fa-fw mr-3"></span>
+                <span class="menu-collapsed">Projetos</span>
+            </div>
+        </a>
+
+        <a href="#" class="bg-dark list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-calendar fa-fw mr-3"></span>
+                <span class="menu-collapsed">Cursos</span>
+            </div>
+        </a>
+        
+        <a href="#" class="bg-dark list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-image fa-fw mr-3"></span>
+                <span class="menu-collapsed">Galeria</span>
+            </div>
+        </a>
+        <a href="#" data-toggle="sidebar-colapse" class="bg-dark list-group-item list-group-item-action d-flex align-items-center">
+            <div class="d-flex w-100 justify-content-start align-items-center">
+                <span class="fa fa-file fa-fw mr-3"></span>
+                <span id="collapse-text" class="menu-collapsed">Processo Seletivo</span>
+            </div>
+        </a>
+    </ul><!-- List Group END-->
+</div><!-- sidebar-container END -->
