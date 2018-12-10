902c0596c06686c7dbddd5324d083d9ef8994f68
Bruno Morii <brunomorii.1998@gmail.com>
2018-12-10 19:26:06 -0200

    Adicoes membros

--- resources/views/dashboard/membros/index.blade.php
+++ resources/views/dashboard/membros/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<section id="cursos_db_home">
+<section id="membros_index">
     <div class="container">
-        <h1>Cursos</h1>
+        <h1>Membros</h1>
         <p class="text-justify">Área destinada à edição de membros<br>
         Você pode verificar os membros e editar seus dados ou criar </p><a class="btn btn-secondary" href="{{ route('membros.create') }}">Novo Membro</a>
 
@@ -13,27 +13,27 @@
             <table class="table table-striped table-hover">
                 <thead>
                     <tr>
-                        <th scope="col">Curso</th>
-                        <th scope="col">Data</th>
-                        <th scope="col">Finalizado</th>
-                        <th scope="col">Com Vagas</th>
-                        <th scope="col">Pago</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Editar</th>
+                        <th scope="col">Excluir</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Curso 1</td>
-                        <td>XX/XX/XX</td>
+                        <td style="text-align: center;"><img src="{{asset ('img/placeholders/no_photo.jpg')}}" style="height: 7%;"></td>
+                        <td>Batatinha 1</td>
                         <td>Sim</td>
-                        <td>Sim</td>
-                        <td>Sim</td>
+                        <td style="text-align: center;"><img src="{{asset ('img/icones/editar.png')}}" style="height: 25px;"></td>
+                        <td style="text-align: center;"><img src="{{asset ('img/icones/excluir.png')}}" style="height: 25px"></td>
                     </tr>
                     <tr>
-                        <td>Curso 2</td>
-                        <td>XX/XX/XX</td>
+                        <td style="text-align: center;"><img src="{{asset ('img/placeholders/no_photo.jpg')}}" style="height: 7%;"></td>
+                        <td>Batatinha 2</td>
                         <td>Não</td>
-                        <td>Não</td>
-                        <td>Não</td>
+                        <td style="text-align: center;"><img src="{{asset ('img/icones/editar.png')}}" style="height: 25px;"></td>
+                        <td style="text-align: center;"><img src="{{asset ('img/icones/excluir.png')}}" style="height: 25px"></td>
                     </tr>
                 </tbody>
             </table>
