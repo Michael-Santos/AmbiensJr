c15cf92ad5996903b97d5ba361eb821f6a0c4969
Bruno Morii <brunomorii.1998@gmail.com>
2018-12-10 16:54:05 -0200

    Organização das pastas

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -4,8 +4,100 @@
 	<!-- EQUIPE -->
 	<section id="team" class="pb-5">
 	    <div class="container">
+
+            <!-- TÍTULO -->
 	        <h5 class="section-title h1">NOSSA EQUIPE</h5>
+
+            <!-- IMAGEM DE TOPO DA EQUIPE -->
 	        <img src="{{asset ('img/placeholders/equipe.png')}}" style="width: 100%; margin-bottom: 10px;">
+
+            <!-- HEADER INDICIANDO SETOR -->
+            <div class="card-header text-center text-white" style = 'background-color: #33AA33; margin-bottom: 15px;'>
+                <h2>Setor</h2>
+            </div>
+
+            <!-- MEMBROS DO SETOR -->
+            <div class="row">
+                <!-- Membro -->
+            <div class="col-sm-6 mb-3">
+                <div class="card shadow">
+                    <div class="row card-body">
+                        <div class="col-sm-3">
+                            <img class="rounded-circle" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" alt="Generic placeholder image" width="120" height="120">
+                        </div>
+                        <div class = "col-sm-9 mt-3">
+                            <h5 class="card-title">Nome</h5>
+                            <p class="card-text">
+                                Descrição
+                                <br>
+                                Descrição
+                            </p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <!-- /Membro -->
+
+            <!-- Membro -->
+            <div class="col-sm-6 mb-3">
+                <div class="card shadow">
+                    <div class="row card-body">
+                        <div class="col-sm-3">
+                            <img class="rounded-circle" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" alt="Generic placeholder image" width="120" height="120">
+                        </div>
+                        <div class = "col-sm-9 mt-3">
+                            <h5 class="card-title">Nome</h5>
+                            <p class="card-text">
+                                Descrição
+                                <br>
+                                Descrição
+                            </p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <!-- /Membro -->
+            <!-- Membro -->
+            <div class="col-sm-6 mb-3">
+                <div class="card shadow">
+                    <div class="row card-body">
+                        <div class="col-sm-3">
+                            <img class="rounded-circle" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" alt="Generic placeholder image" width="120" height="120">
+                        </div>
+                        <div class = "col-sm-9 mt-3">
+                            <h5 class="card-title">Nome</h5>
+                            <p class="card-text">
+                                Descrição
+                                <br>
+                                Descrição
+                            </p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <!-- /Membro -->
+            <!-- Membro -->
+            <div class="col-sm-6 mb-3">
+                <div class="card shadow">
+                    <div class="row card-body">
+                        <div class="col-sm-3">
+                            <img class="rounded-circle" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" alt="Generic placeholder image" width="120" height="120">
+                        </div>
+                        <div class = "col-sm-9 mt-3">
+                            <h5 class="card-title">Nome</h5>
+                            <p class="card-text">
+                                Descrição
+                                <br>
+                                Descrição
+                            </p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <!-- /Membro -->
+            </div>
+
+
 	        <div class="row">
 	            <!-- MEMBRO -->
 	            <div class="col-xs-12 col-sm-6 col-md-4">
