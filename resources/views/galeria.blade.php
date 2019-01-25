c3b41ac7524b72454ce480b3977deddae7e68547
Antonio <ajmedeiros93@gmail.com>
2019-01-25 03:16:26 -0200

    galeria e equipe estatico, slide ta com problema

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -8,9 +8,9 @@
 					<!-- Elemento dinâmico (SELECT FROM BD)-->
 					<a class = "col-lg-3 col-md-4 col-xs-6 thumb" href = "{{ route('album') }}">
 						<div class="card bg-dark text-white text-center">
-							<img class="card-img" src="https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg" alt="Card image">
+							<img class="card-img" src="{{asset ('img/galeria/teste/5.png')}}" alt="Card image">
 							<div class="card-img-overlay">
-								<h4 class="card-title">Doguíneos</h4>
+								<h4 class="card-title">Álbum com muito sapo</h4>
 							</div>
 						</div>
 					</a>		
