3c6247541d37f89fa25e22ade9e5f6e7e1b66ce2
Bruno Morii <brunomorii.1998@gmail.com>
2019-01-25 11:24:03 -0200

    Arrumando home do admin

--- resources/views/dashboard/home_dash.blade.php
+++ resources/views/dashboard/home_dash.blade.php
@@ -11,8 +11,9 @@
         <p>Sinta-se à vontade para navegar pelo menu ao lado. <br>
         Ou então, clique no botão abaixo para ir à Página Inicial do Site Público</p>
         <a class="btn btn-outline-dark" href="{{ url('teste') }}">Site Principal</a>
+        <!--
         <p class="mt-3">Caso deseje alterar seus dados visite seu perfil clicando em sua foto no topo do Dashboard ou pelo botão abaixo:</p>
-        <a class="btn btn-outline-dark mx-0" href="#">Meu Perfil</a>
+        <a class="btn btn-outline-dark mx-0" href="#">Meu Perfil</a>-->
 
     </div>
 </section>
